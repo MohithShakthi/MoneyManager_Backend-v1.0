56a3c891605b598ca4308e02d969a1b1c825d0ff
Mohith Shakthi <mohithshakthikv2001@gmail.com>
2025-11-22 16:04:01 +0530

    added excel and email service

--- moneymanager/src/main/java/com/mohith/moneymanager/service/EmailService.java
+++ moneymanager/src/main/java/com/mohith/moneymanager/service/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Base64;
 import java.util.Map;
 
 @Service
@@ -48,4 +49,49 @@ public class EmailService {
                 .onErrorReturn("Failed to send email")
                 .block();
     }
+
+    public String sendEmailWithAttachment(
+            String to,
+            String subject,
+            String body,
+            byte[] attachment,
+            String filename
+    ) {
+
+        String url = "https://api.brevo.com/v3/smtp/email";
+
+        // Base64 encode the file (Brevo requirement)
+        String base64Attachment = Base64.getEncoder().encodeToString(attachment);
+
+        Map<String, Object> content = Map.of(
+                "sender", Map.of(
+                        "name", senderName,
+                        "email", senderEmail
+                ),
+                "to", new Object[]{
+                        Map.of("email", to)
+                },
+                "subject", subject,
+                "htmlContent", body,
+
+                // Brevo requires attachments as base64 objects
+                "attachment", new Object[]{
+                        Map.of(
+                                "name", filename,
+                                "content", base64Attachment
+                        )
+                }
+        );
+
+        return webClient.post()
+                .uri(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("api-key", apiKey)
+                .bodyValue(content)
+                .retrieve()
+                .bodyToMono(String.class)
+                .onErrorReturn("Failed to send email")
+                .block();
+    }
+
 }
